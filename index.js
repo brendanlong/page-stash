563dcfa8b88355a14ce2f3f993f0a99104bf00f5
Brendan Long <self@brendanlong.com>
2016-12-07 18:08:29 -0500

    Add routes and start on database models.

--- index.js
+++ index.js
@@ -2,17 +2,26 @@
 "use strict";
 const yargs = require("yargs");
 
-const app = require("./lib/app");
-const log = require("./lib/log");
+const config = require("./lib/config");
 
 
 const args = yargs
+    .option("d", {
+        alias: "db-uri",
+        default: "sqlite://:memory:",
+        type: "string",
+    })
     .option("p", {
         alias: "port",
         default: 8000,
         type: "number",
     })
     .argv;
+
+config.dbUri = args.dbUri;
+
+const app = require("./lib/app");
+const log = require("./lib/log");
 
 app.listen(args.port, () => {
     log.info({port: args.port}, "Listening on port %s", args.port);
